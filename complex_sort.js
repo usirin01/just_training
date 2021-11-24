f7b52516e37ce1f084d742581c1a01b046f9e390
usirin01 <usirin2021@gmail.com>
2021-11-24 12:58:54 +0100

    added new project

--- complex_sort.js
+++ complex_sort.js
@@ -4,6 +4,7 @@ function PushButton()
 var a = document.getElementById("number-1").value;
 var b = document.getElementById("number-2").value;
 var c = document.getElementById("number-3").value;
+var d = document.getElementById("number-4").value;
 
 var dizi = new Array(a, b, c);
 document.write(dizi.sort(function(a,b){return b-a}));
