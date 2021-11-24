3baa90458dc6cb9a567a7d3333f7cf10a0a9b6f9
usirin01 <usirin2021@gmail.com>
2021-11-24 13:13:12 +0100

    small mistakes fixed

--- complex_sort.js
+++ complex_sort.js
@@ -6,7 +6,7 @@ var a = document.getElementById("number-1").value;
 var b = document.getElementById("number-2").value;
 var c = document.getElementById("number-3").value;
 var d = document.getElementById("number-4").value;
-var e = document.getElementById("number-4").value;
+var e = document.getElementById("number-5").value;
 
 var dizi = new Array(a, b, c, d, e);
 document.write(dizi.sort(function(a,b){return b-a}));
