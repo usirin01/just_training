f7a42902a7ba8570d4960e050445d18e9aff29e9
usirin01 <usirin2021@gmail.com>
2021-11-24 13:01:46 +0100

    added code for fifth number

--- complex_sort.js
+++ complex_sort.js
@@ -5,6 +5,7 @@ var a = document.getElementById("number-1").value;
 var b = document.getElementById("number-2").value;
 var c = document.getElementById("number-3").value;
 var d = document.getElementById("number-4").value;
+var d = document.getElementById("number-4").value;
 
 var dizi = new Array(a, b, c);
 document.write(dizi.sort(function(a,b){return b-a}));
