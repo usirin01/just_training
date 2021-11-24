84b9a9ff06f838322faa724a5a730fe56a95edd0
usirin01 <usirin2021@gmail.com>
2021-11-24 13:04:05 +0100

    last two numbers variable added into the function

--- complex_sort.js
+++ complex_sort.js
@@ -1,3 +1,4 @@
+//function structure was created
 function PushButton()
 {
 
@@ -5,9 +6,9 @@ var a = document.getElementById("number-1").value;
 var b = document.getElementById("number-2").value;
 var c = document.getElementById("number-3").value;
 var d = document.getElementById("number-4").value;
-var d = document.getElementById("number-4").value;
+var e = document.getElementById("number-4").value;
 
-var dizi = new Array(a, b, c);
+var dizi = new Array(a, b, c, d, e);
 document.write(dizi.sort(function(a,b){return b-a}));
 console.log(dizi);
 
