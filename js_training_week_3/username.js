fdec0c35b560e3ab67e59c06413ebe084645aa3c
usirin01 <usirin2021@gmail.com>
2021-12-08 23:19:35 +0100

    username was uploaded

---
+++ js_training_week_3/username.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+
+
+let btnLogin = document.querySelector("#login");
+btnLogin.addEventListener("click", function(event){
+
+    const name = document.querySelector("name");
+    const password = document.querySelector("password");
+   
+    alert("correct");
+
+})
